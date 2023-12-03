a6af12301561f4d50ad54dd8d1699734eac2286f
JeongJaemin <woalsdl0439@gmail.com>
2023-12-03 19:06:58 +0900

    폴더 정리

---
+++ Project-S/Assets/Resources/Script/Manager/AddressbleManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+
+public class AddressbleManager : Singleton<AddressbleManager>
+{
+    private Dictionary<string, Object> _bundleObjs = new Dictionary<string, Object>();
+
+    public override void Init()
+    {
+
+    }
+
+    public void LoadAssetAsync<T>(string assetName) where T : Object //에셋 로드
+    {
+        _bundleObjs.TryGetValue(assetName, out Object obj);
+
+        if (obj) // 로드가 완료된 에셋
+        {
+            
+        }
+        else 
+        {
+            Addressables.LoadAssetAsync<T>(assetName).Completed += handle =>
+            {
+                switch (handle.Status)
+                {
+                    case AsyncOperationStatus.Succeeded:
+                        {
+                            obj = handle.Result;
+                            _bundleObjs.Add(assetName, obj);
+                        }
+                        break;
+                    case AsyncOperationStatus.Failed:
+
+                        break;
+                }
+            };
+        }
+    }
+
+    public T InstantiateOrNull<T>(string key, Transform parentTrans = null) where T : Object // 로드된 에셋 생성
+    {
+        if (_bundleObjs.TryGetValue(key, out Object obj))
+        {
+            if(obj)
+            {
+                return Instantiate((T)obj, parentTrans);
+            }
+        }
+
+        return null;
+    }
+
+    public void SetSprite(Image image, string spriteName)
+    {
+        Addressables.LoadAssetAsync<Sprite>(spriteName).Completed += handle =>
+        {
+            image.sprite = handle.Result;
+        };
+    }
+
+    public void Start()
+    {
+        //LoadAssetAsync<Sprite>("Char/Skunk2");
+        //LoadAssetAsync<Sprite>("Char/Wolf");
+
+    }
+
+    public void CreateBox()
+    {
+        InstantiateOrNull<GameObject>("Box");
+    }
+}
